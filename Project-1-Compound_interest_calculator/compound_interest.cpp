789ef1431edc6ffd5e1b1a4b350bb7cfaa7f5860
Matteo Ferrea <matteo.ferrea@stellantis.com>
2024-09-05 08:34:29 +0200

    Fixed initial comment

--- Project-1-Compound_interest_calculator/compound_interest.cpp
+++ Project-1-Compound_interest_calculator/compound_interest.cpp
@@ -1,3 +1,4 @@
+/*
 # Project: Compounded Interest Calculator
 # Filename: compound_interest.cpp
 # Copyright (C) 2024 Matteo Ferrea
@@ -14,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+*/
 #include <iostream>
 #include <cmath>
 
@@ -28,9 +29,9 @@ int main() {
         std::cin >> choice;
         std::cout << "\n";
         if (choice == 1) {
-	    std::cout << "* Interest calculation *\n";
+            std::cout << "* Interest calculation *\n";
             calculateInterest();
-	    std::cout << std::endl;
+            std::cout << std::endl;
         } else if (choice == 2) {
             std::cout << "Exiting the program." << std::endl;
             break;
@@ -49,20 +50,24 @@ void showMenu() {
 }
 
 void calculateInterest() {
+    double initial_amount;
     double monthly_deposit;
     double annual_interest;
     int years;
-    double current_capital = 0;
+    double current_capital;
     double total_deposited = 0;
 
+    std::cout << "Initial amount: ";
+    std::cin >> initial_amount;
     std::cout << "Years to calculate interest: ";
     std::cin >> years;
     int months = years * 12;
-    std::cout << "Enter the monthly deposit: ";
+    std::cout << "Enter the monthly deposit (enter 0 to avoid monthly deposits): ";
     std::cin >> monthly_deposit;
     std::cout << "Enter the annual interest rate (in decimal, e.g., 0.03 for 3%): ";
     std::cin >> annual_interest;
 
+    current_capital = initial_amount;
     double monthly_interest = std::pow(1 + annual_interest, 1.0 / 12.0) - 1;
 
     for (int i = 0; i < months; i++) {
@@ -72,7 +77,7 @@ void calculateInterest() {
 
         if ((i + 1) % 12 == 0) {
             std::cout << "After " << (i + 1) / 12 << " years" << std::endl;
-            std::cout << "Total deposited: " << total_deposited << std::endl;
+            std::cout << "Total deposited: " << total_deposited + initial_amount << std::endl;
             std::cout << "Total capital: " << current_capital << std::endl;
             std::cout << "----------------------" << std::endl;
         }
@@ -80,9 +85,9 @@ void calculateInterest() {
 
     std::cout << "\n######################\n";
     std::cout << "Final result after " << years << " years: " << std::endl;
-    std::cout << "Total deposited: " << total_deposited << std::endl;
+    std::cout << "Total deposited: " << total_deposited + initial_amount << std::endl;
     std::cout << "Final capital: " << current_capital << std::endl;
-    std::cout << "Interest earned: " << current_capital - total_deposited << std::endl;
-    std::cout << "Overall gain compared to total deposit: " << (current_capital - total_deposited) / (total_deposited / 100) << "%" << std::endl;
+    std::cout << "Interest earned: " << current_capital - (total_deposited + initial_amount) << std::endl;
+    std::cout << "Overall gain compared to total deposit: " << (current_capital - (total_deposited + initial_amount)) / ((total_deposited + initial_amount) / 100) << "%" << std::endl;
     std::cout << "######################\n";
 }
